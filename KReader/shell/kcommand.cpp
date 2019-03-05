571f5489967deab4e9feabc9fe9faddc346fa1f8
CryFeiFei <809151882@qq.com>
2019-03-05 23:26:28 +0800

    [删除代码][删除设置icon的代码]

--- KReader/shell/kcommand.cpp
+++ KReader/shell/kcommand.cpp
@@ -16,6 +16,11 @@ void kcommand::setText(const QString& text)
 	m_text = text;
 }
 
+void kcommand::setText(QString&& text)
+{
+	m_text = std::move(text);
+}
+
 bool kcommand::visible() const
 {
 	return  m_visible;
@@ -25,3 +30,18 @@ void kcommand::setVisible(bool visible)
 {
 	m_visible = visible;
 }
+
+//QIcon kcommand::icon() const
+//{
+//	return m_icon;
+//}
+
+//void kcommand::setIcon(const QIcon& icon)
+//{
+//	m_icon = icon;
+//}
+
+//void kcommand::setIcon(QIcon&& icon)
+//{
+//	m_icon = std::move(icon);
+//}
--- KReader/shell/kcommand.h
+++ KReader/shell/kcommand.h
@@ -1,4 +1,7 @@
-﻿// command means
+﻿// MIT License
+// auther zhangpengfei
+// email zhangpf0313@gmail.com
+
 #ifndef KCOMMAND_H
 #define KCOMMAND_H
 
@@ -13,31 +16,37 @@ class kcommand : public QObject
 	Q_PROPERTY(QString text MEMBER m_text NOTIFY textChange)
 	Q_PROPERTY(bool visible READ visible WRITE setVisible)
 	Q_PROPERTY(bool visible MEMBER m_visible NOTIFY visibleChange)
-	Q_PROPERTY(QIcon icon READ icon WRITE setIcon)
-	Q_PROPERTY(QIcon icon MEMBER m_icon NOTIFY iconChange)
+//	Q_PROPERTY(QIcon icon READ icon WRITE setIcon)
+//	Q_PROPERTY(QIcon icon MEMBER m_icon NOTIFY iconChange)
+	using QObject::QObject;
 public:
 	explicit kcommand(QObject* host = nullptr, QObject *parent = nullptr);
 
 public:
 	QString text() const;
 	void setText(const QString& text);
+	void setText(QString&& text);
 
 	bool visible() const;
 	void setVisible(bool visible);
+
+//	QIcon icon() const;
+//	void setIcon(const QIcon& icon);
+//	void setIcon(QIcon&& icon);
 
 
 signals:
 	void textChange();
 	void visibleChange();
-	void iconChange();
+//	void iconChange();
 
 public slots:
 
 private:
+	QObject* m_host; //这个应该是主界面的
 	QString m_text;
 	bool m_visible;
 	QIcon m_icon;
-	QObject* m_host;
 };
 
 #endif // KCOMMAND_H
